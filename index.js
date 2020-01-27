4f9db9707eaca4d450c92f9a792b6aa3de533474
Kaan <kkureli@gmail.com>
2020-01-27 13:49:36 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution here
+
+function findMatching(arr, str) {
+    return arr.filter(n => {
+        return n.toUpperCase() === str.toUpperCase();
+    })
+}
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(n => {
+        return n[0] === str[0]
+    })
+}
+
+function matchName(arr, str) {
+    return arr.filter(n => {
+        return n.name === str
+    })
+}
+
+const drivers = [{
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+];
+
+watchName(drivers, "Bobby")
