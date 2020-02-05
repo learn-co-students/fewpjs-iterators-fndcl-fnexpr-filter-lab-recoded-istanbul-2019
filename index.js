ea2196cb3dc48eb139dbe23ca34d2df16c8e7bfb
Oktay Ibis <oktayibis@gmail.com>
2020-02-05 23:43:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(array, string) {
+   let result = array.filter(driver => {
+        return driver.toLowerCase() === string.toLowerCase();
+    })
+
+    return result;
+}
+
+function fuzzyMatch(array, string){
+
+    let result = array.filter(driver => {
+        return driver.startsWith(string);
+    })
+
+    return result;
+}
+
+function matchName(drivers, string){
+    let result = drivers.filter( d => {
+       return d.name === string
+    })
+
+    return result;
+}
