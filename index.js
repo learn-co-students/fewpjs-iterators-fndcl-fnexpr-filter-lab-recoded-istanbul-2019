d48c84d2e9957d1c4aaaf52e7181838aa850cbcf
basil-kawak <basilalisa0@gmail.com>
2020-04-27 01:10:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+// let even = arr.filter(n => {
+//     return n % 2 === 0;
+//   })
+
+
+
+function findMatching(driver, name) {
+   return driver.filter(x => new RegExp(name, 'i').test(x));
+}
+
+function fuzzyMatch(driver, name) {
+    return driver.filter(x => new RegExp(`^${name}`, 'i').test(x));
+}
+
+function matchName(driver, name) {
+    return driver.filter(x => new RegExp(name, 'i').test(x.name));
+}
