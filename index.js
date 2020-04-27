ddff712b75f905a036b6e69ce770b16334a4a946
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-04-27 18:01:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+
+function findMatching(array,string){
+ array.filter(n => {
+  return n= 'string';
+});
+}
+
+// function findMatching(array){
+
+//     for(var i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 === 0) even.push(arr[i]);
+//       }
+// }
