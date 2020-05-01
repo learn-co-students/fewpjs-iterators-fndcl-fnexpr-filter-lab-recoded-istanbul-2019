75d445e848d9985e87505da8929ba6cb73f70b0a
emrerdem1 <emrerdem94@gmail.com>
2020-05-01 19:21:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+let string = "Bobby";
+const drivers1 = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+const findMatching = (drivers, string) => drivers.filter(name => name.toLowerCase() == string.toLowerCase());
+findMatching(drivers1, string);
+
+const fuzzyMatch = (drivers, string) => drivers.filter(name => name.charAt(0) == string.charAt(0));
+fuzzyMatch(drivers1, string);
+
+const drivers = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+];
+
+const matchName = (drivers, string) => {
+    return drivers.filter(obj => (obj.name == string));
+
+}
+matchName(drivers, string)
