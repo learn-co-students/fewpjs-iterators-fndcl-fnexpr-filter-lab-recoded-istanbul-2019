4c9a2cbfde6b679e6639415a34209463b2cbafe5
kamalbakour <kamal.bak92@gmail.com>
2020-05-15 00:30:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers,street){
+    return  drivers.filter(function(array){
+      return array.toLowerCase()===street.toLowerCase();
+    });
+  }
+  
+  function fuzzyMatch(driversA, streetA){
+    
+    return driversA.filter(function(array){
+      return array.charAt(0) === streetA.charAt(0);
+      
+    })
+  }
+  
+  function matchName(driversB, streetB){
+    return driversB.filter(function(a){
+      return a.name === streetB;
+    })
+  }
