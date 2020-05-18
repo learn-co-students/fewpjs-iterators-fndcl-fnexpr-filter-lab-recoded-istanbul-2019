d9632fddb6a5e1370c1b40a748be2aebe5c3cfec
Hassan <github email address>
2020-05-18 20:26:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+findMatching = (drivers, string) => {
+    const regex = new RegExp (string, "i");
+    const res = drivers.filter(name => {
+       return regex.test(name);
+    });
+    return res;
+}
+
+function fuzzyMatch (drivers, string){
+    let regex = new RegExp (`^${string}`, 'i');
+    let res = drivers.filter (name => {
+        return regex.test(name);
+    });
+    return res;
+}
+
+function matchName (drivers, string){
+    let regex = new RegExp (string, 'i');
+    let res = drivers.filter (name => {
+        return regex.test(name.name);
+    });
+    return res;
+}
